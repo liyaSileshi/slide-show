2ea4388827480664804294321ade8bb5f3fb2955
liyaSileshi <liyasileshi@gmail.com>
2019-11-18 22:51:21 -0800

    added 2 more slides

--- ms-slide-show.js
+++ ms-slide-show.js
@@ -1,6 +1,9 @@
 (function(){
     function makeSlideshow(slides){
         // const slides = document.getElementById(slidesId)
+        const prev = document.getElementsByClassName('prev-button')
+        console.log(prev)
+        const next = document.getElementsByClassName('next-button')
         const slidesInner = slides.querySelector('.slides-inner')
         const images = slidesInner.querySelectorAll('img')
     
@@ -9,9 +12,26 @@
         slidesInner.style.transition = `${transition}ms`
     
         const slidesWidth = slides.clientWidth
+        const slidesHeight = slides.clientHeight
         let index = 0
+        prev.onclick = function(e){
+          console.log('prev')
+          if (index == 0) {
+            index = images.length - 1
+          } else {
+            index -= 1
+          }
+          
+          slidesInner.style.transform = `translate3d(${index * -slidesWidth}px, 0, 0)`
+        }
+        next.onclick = function(e){
+          index += 1
+          slidesInner.style.transform = `translate3d(${index * -slidesWidth}px, 0, 0)`
+        }
         // setInterval(callback, timeMS)
         setInterval(function(){
+          
+          
           index += 1
           if(index === images.length){ 
             index = 0
